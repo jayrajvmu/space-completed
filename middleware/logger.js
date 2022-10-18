cfe79f71d1cf29045f794b78c67e74cbbc05abf0
bhargav_ultiweb <bargava.narasimhan@ultiweb.in>
2022-10-18 10:58:23 +0530

    🗑️ Remove old code

--- middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-const logger = (request, response, next) => {
-	console.log(`${moment().format()} ${request.method} ${request.protocol}://${request.get('host')}${request.originalUrl}`);
-	next();
-}
-
-module.exports = logger;
