becc7f2e4cd232851eb4652f6260018612b9c8cb
Vijay <vijaybilla897@gmail.com>
2022-10-24 14:00:56 +0530

    added wing Delete

--- js/wingEdit.js
+++ js/wingEdit.js
@@ -1,29 +1,4 @@
-function updateTable() {
-    let tableId=document.getElementById("tableId");
-    let seatCount = document.getElementById("seatCount");
-    let adminCode = document.getElementById("adminCode");
-    let message= document.getElementById("message");
-
-    let updateWSTable = {
-        tValue: `${tableId.value}`,
-        sValue: `${seatCount.value}`,
-        avalue: `${adminCode.value}`,
-    }
-    console.log(updateWSTable);
-    axios.put('http://localhost:5500/update', updateWSTable)
-                .then(response => {
-                    console.log(response.data)
-                    tableId.value="";
-                    seatCount.value="";
-                    adminCode.value="";
-                })
-                .catch((err)=>{
-                    console.log(err);
-                });
-   axios.get('http://localhost:5500/updated').then(
-    response =>{
-        message.textContent = response.data;
-        console.log(response.data);
-    }
-   )
+function openingWingDelete() {
+    let wingDeleting = document.getElementById("wingDeleting");
+    wingDeleting.style.display="block"
 }
