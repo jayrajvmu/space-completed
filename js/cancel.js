c74d62517c975168d1fed9b2fd35f54ba6d9929b
Haribabu <haribabudiguvinti@gmail.com>
2022-10-29 12:55:32 +0530

    if else conditions added into cancel js file

--- js/cancel.js
+++ js/cancel.js
@@ -11,39 +11,46 @@ const bookedSeats = async () => {
     });
     console.log(response.data.data);
     let blogDatas = response.data.data;
-   
-    let html = blogDatas
-      .map((blog) => {
-        console.log(blog);
-        
-        return `
-        <div class="booked-seats_col">
-        <div class="booked-seats_container">
-          <div class="booked-seats_details">
-            <div class="booked-seats_input">Desk Id :</div>
-            <div class="booked-seats_value">${blog.seat_id}</div>
-          </div>
-          <div class="booked-seats_details">
-            <div class="booked-seats_input">Employee Id :</div>
-            <div class="booked-seats_value">${blog.emp_id}</div>  
-          </div>
-          <div class="booked-seats_details">
-            <div class="booked-seats_input">Booked Date :</div>
-            <div class="booked-seats_value">${new Date(blog.date).toISOString().split('T')[0]}</div>
-          </div>
-          <div class="booked-seats_details">
-            <div class="booked-seats_input">Shift :</div>
-            <div class="booked-seats_value">${blog.shift_id}</div>
-          </div>
-          <div class="form-submit">
-            <button type="submit" class="button123" onclick="cancelBookedSeat('${blog.id}')">Cancel Seat</button>
-            <button type="submit" class="button1234"  onclick="checkinUser('${blog.id}')">Check-in</button>
-          </div>
+    let html ;
+    if (blogDatas.length === 0) {
+      html = 'No Seats Booking'
+  } else{
+    html = blogDatas
+    .map((blog) => {
+      console.log(blog);
+      
+      return `
+      <div class="booked-seats_col">
+      <div class="booked-seats_container">
+        <div class="booked-seats_details">
+          <div class="booked-seats_input">Desk Id :</div>
+          <div class="booked-seats_value">${blog.seat_id}</div>
+        </div>
+        <div class="booked-seats_details">
+          <div class="booked-seats_input">Employee Id :</div>
+          <div class="booked-seats_value">${blog.emp_id}</div>  
+        </div>
+        <div class="booked-seats_details">
+          <div class="booked-seats_input">Booked Date :</div>
+          <div class="booked-seats_value">${new Date(blog.date).toISOString().split('T')[0]}</div>
+        </div>
+        <div class="booked-seats_details">
+          <div class="booked-seats_input">Shift :</div>
+          <div class="booked-seats_value">${blog.shift_id}</div>
+        </div>
+        <div class="form-submit">
+          <button type="submit" class="button123" onclick="cancelBookedSeat('${blog.id}')">Cancel Seat</button>
+          <button type="submit" class="button1234"  onclick="checkinUser('${blog.id}')">Check-in</button>
         </div>
       </div>
-        `;
-      })
-      .join("");
+    </div>
+      `;
+    })
+    .join("");
+
+  }
+   
+    
     blogRow.insertAdjacentHTML("afterbegin", html);
 
     //cancellation module starts//
