720fc8ea12bf8bd512a7ff3d69385aacab01cb9e
devganesh123 <86190761+devganesh123@users.noreply.github.com>
2022-10-31 04:20:21 +0530

    ✨date filter completed

--- public/js/seat-two.js
+++ public/js/seat-two.js
@@ -1,8 +1,14 @@
+//set date into current date
+$(document).ready(function () {
+  $("#date").datepicker();
+  $("#date").datepicker("setDate", "12/31/2000");
+});
+
 let fetchUrl = "http://127.0.0.1:5000/availability/dates";
 
 let grid = document.querySelector("#cont");
 let filterByWing = document.getElementById("wing");
-let filterByDate = document.getElementById("wdate");
+let filterByDate = document.getElementById("date");
 
 fetch(fetchUrl)
   .then((res) => res.json())
@@ -19,31 +25,21 @@ fetch(fetchUrl)
 
 //filterInput.addEventListener("keyup", filterProducts);
 filterByWing.addEventListener("change", filterProducts);
-filterByDate.addEventListener("change", () => {
-  let dateFilter = filterByDate.value;
-  console.log(dateFilter);
+filterByDate.addEventListener("change", filterDate);
 
+function filterDate() {
+  let dateFilter = filterByDate.value;
   let tableItems = document.querySelectorAll(".wing-table");
 
   for (let tableItem of tableItems) {
-    let dateItems = document.querySelectorAll(`.seat-date-${dateFilter}`);
-    for (let dateItem of dateItems) {
-      if (tableItem.contains(dateItem)) {
-        console.log(tableItem);
-        for (let tableItem of tableItems) {
-          tableItem.style.display = "none";
-        }
-        tableItem.style.display = "block";
-      } else {
-        console.log(tableItem);
-        for (let tableItem of tableItems) {
-          tableItem.style.display = "block";
-        }
-        tableItem.style.display = "none";
-      }
+    let dateItem = tableItem.querySelector(`.date-${dateFilter}`);
+    if (dateItem != null) {
+      tableItem.style.display = "block";
+    } else {
+      tableItem.style.display = "none";
     }
   }
-});
+}
 
 //callback function
 function filterProducts() {
@@ -55,13 +51,13 @@ function filterProducts() {
   console.log(wings);
 
   //* filter by wing name
-  // for (let i = 0; i < wings.length; i++) {
-  //   if (wings[i].classList.contains(`${wingFilter}`)) {
-  //     wings[i].style.display = "initial";
-  //   } else {
-  //     wings[i].style.display = "none";
-  //   }
-  // }
+  for (let i = 0; i < wings.length; i++) {
+    if (wings[i].classList.contains(`${wingFilter}`)) {
+      wings[i].style.display = "initial";
+    } else {
+      wings[i].style.display = "none";
+    }
+  }
 }
 
 // get value from the api create dynamic element
@@ -105,7 +101,7 @@ function addElement(appendIn, value, index) {
         seatDiv.innerHTML = `
           <div class='seat-col'>
           <div class='seat-name ${seatName}'>${seatName}</div>
-          <div class='seat-date-${date}'>${date}</div>
+          <div class='seat-date date-${date}'>${date}</div>
           <div class='seat-shift ${shiftName}'>${shiftName}</div>
           <div class='seat-availability'>${availability}</div>
           </div>`;
@@ -113,106 +109,106 @@ function addElement(appendIn, value, index) {
       });
     });
   });
+}
 
-  let seatItems = document.querySelectorAll(".chair");
-  let modalContainer = document.querySelector("#modal-section");
-  let closeBtn = document.querySelector("#close-btn");
-  let submitBtn = document.querySelector("#submit-btn");
-  let cancelBookingBtn = document.querySelector("#cancel-booking");
-  const overlay = document.getElementById("overlay");
-  let message = document.querySelector("#message");
+let seatItems = document.querySelectorAll(".chair");
+let modalContainer = document.querySelector("#modal-section");
+let closeBtn = document.querySelector("#close-btn");
+let submitBtn = document.querySelector("#submit-btn");
+let cancelBookingBtn = document.querySelector("#cancel-booking");
+const overlay = document.getElementById("overlay");
+let message = document.querySelector("#message");
 
-  seatItems.forEach((item) => {
-    item.onclick = () => setModal(item);
-  });
+seatItems.forEach((item) => {
+  item.onclick = () => setModal(item);
+});
 
-  function setModal(seatItem) {
-    // open the modal
-    modalContainer.classList.add("show");
-    overlay.classList.add("active");
+function setModal(seatItem) {
+  // open the modal
+  modalContainer.classList.add("show");
+  overlay.classList.add("active");
 
-    //  insert desk id
-    let deskInput = document.querySelector("#desk-id");
+  //  insert desk id
+  let deskInput = document.querySelector("#desk-id");
 
-    //getting the value of the seat name
-    let seatChildren = seatItem.children;
-    for (let node of seatChildren) {
-      let seatChlid = node.children;
-      for (let seatNextChild of seatChlid) {
-        if (seatNextChild.classList.contains("seat-name")) {
-          deskInput.value = seatNextChild.textContent;
-        }
+  //getting the value of the seat name
+  let seatChildren = seatItem.children;
+  for (let node of seatChildren) {
+    let seatChlid = node.children;
+    for (let seatNextChild of seatChlid) {
+      if (seatNextChild.classList.contains("seat-name")) {
+        deskInput.value = seatNextChild.textContent;
       }
     }
-
-    // form submit function
-    submitBtn.onclick = () => {
-      postData(seatItem);
-    };
   }
 
-  function postData(seat) {
-    //post endpoint
-    const postUrl = "http://localhost:5000/booking";
-    let desk_id = document.getElementById("desk-id").value;
-    let emp_id = document.getElementById("emp-id").value;
-    let date = document.getElementById("date").value;
-    let shift = document.getElementById("time").value;
-    const payload = {
-      desk_id: `${desk_id}`,
-      emp_id: `${emp_id}`,
-      date: `${date}`,
-      shift: `${shift}`,
-      booked_by: 1,
-      booking_type: 0,
-    };
+  // form submit function
+  submitBtn.onclick = () => {
+    postData(seatItem);
+  };
+}
 
-    axios
-      .post(postUrl, payload)
-      .then((response) => {
-        console.log(response);
-        console.log(response.data);
-        if (response.status === 200) {
-          message.innerHTML = `<p class='${response.data.success}'>${response.data.message}</p>`;
-          if (response.data.success) {
-            seatBooking(seat);
-          }
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+function postData(seat) {
+  //post endpoint
+  const postUrl = "http://localhost:5000/booking";
+  let desk_id = document.getElementById("desk-id").value;
+  let emp_id = document.getElementById("emp-id").value;
+  let date = document.getElementById("date").value;
+  let shift = document.getElementById("time").value;
+  const payload = {
+    desk_id: `${desk_id}`,
+    emp_id: `${emp_id}`,
+    date: `${date}`,
+    shift: `${shift}`,
+    booked_by: 1,
+    booking_type: 0,
+  };
 
-  //close the modal
-  closeBtn.addEventListener("click", closeModal);
-  function closeModal() {
-    modalContainer.classList.remove("show");
-    overlay.classList.remove("active");
-    //reset the input values
-    document.getElementById("desk-id").value = "";
-    document.getElementById("emp-id").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("time").value = "";
-    message.innerHTML = "";
-  }
-
-  function seatBooking(seat) {
-    // close the modal when the response is sent
-    modalContainer.classList.remove("show");
-    overlay.classList.remove("active");
-    message.innerHTML = "";
-
-    // seatItems.forEach((item) => {
-    //   item.classList.remove("booked");
-    // });
-
-    seat.classList.add("booked");
-  }
-
-  function userName() {
-    axios.get(`http://localhost:5000/booking/user/name`).then((response) => {
+  axios
+    .post(postUrl, payload)
+    .then((response) => {
+      console.log(response);
       console.log(response.data);
+      if (response.status === 200) {
+        message.innerHTML = `<p class='${response.data.success}'>${response.data.message}</p>`;
+        if (response.data.success) {
+          seatBooking(seat);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  }
+}
+
+//close the modal
+closeBtn.addEventListener("click", closeModal);
+function closeModal() {
+  modalContainer.classList.remove("show");
+  overlay.classList.remove("active");
+  //reset the input values
+  document.getElementById("desk-id").value = "";
+  document.getElementById("emp-id").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("time").value = "";
+  message.innerHTML = "";
+}
+
+function seatBooking(seat) {
+  // close the modal when the response is sent
+  modalContainer.classList.remove("show");
+  overlay.classList.remove("active");
+  message.innerHTML = "";
+
+  // seatItems.forEach((item) => {
+  //   item.classList.remove("booked");
+  // });
+
+  seat.classList.add("booked");
+}
+
+function userName() {
+  axios.get(`http://localhost:5000/booking/user/name`).then((response) => {
+    console.log(response.data);
+  });
 }
