2d436da72ac1b95162af55cc810436942e31e0d3
devganesh123 <ganesan690101@gmail.com>
2022-11-01 21:49:04 +0530

    cancel js file updated

--- public/js/cancel.js
+++ public/js/cancel.js
@@ -5,6 +5,7 @@ const bookedSeatsUrl = "http://localhost:5000/booking/2";
 let cancellationModal = document.getElementById("modal-section-cancellation");
 let overlayCancellation = document.getElementById("overlay-cancellation");
 
+let signInMessage = document.querySelector("#signin-message");
 const bookedSeats = async () => {
   try {
     const response = await axios(bookedSeatsUrl, {
@@ -21,42 +22,40 @@ const bookedSeats = async () => {
       html = blogDatas
         .map((blog) => {
           console.log(blog);
-          return `
+          blogRow.innerHTML += `
             <div class="booked-seats_col">
               <div class="booked-seats_container">
                 <div class="booked-seats_details">
-                  <div class="booked-seats_input">Desk Id :</div>
-                  <div class="booked-seats_value">${blog.seat_id}</div>
+                  <div class="booked-seats_input">Desk Id </div>
+                  <div class="booked-seats_value">: ${blog.seat_id}</div>
                 </div>
                 <div class="booked-seats_details">
-                  <div class="booked-seats_input">Employee Id :</div>
-                  <div class="booked-seats_value">${blog.emp_id}</div>  
+                  <div class="booked-seats_input">Employee Id  </div>
+                  <div class="booked-seats_value">: ${blog.emp_id}</div>  
                 </div>
                 <div class="booked-seats_details">
-                  <div class="booked-seats_input">Booked Date :</div>
-                  <div class="booked-seats_value">${new Date(
-            blog.date
-          ).toLocaleDateString()}</div>
+                  <div class="booked-seats_input">Booked Date  </div>
+                  <div class="booked-seats_value">: ${new Date(
+                    blog.date
+                  ).toLocaleDateString()}</div>
                 </div>
                 <div class="booked-seats_details">
-                  <div class="booked-seats_input">Shift :</div>
-                  <div class="booked-seats_value">${blog.shift_id}</div>
+                  <div class="booked-seats_input">Shift  </div>
+                  <div class="booked-seats_value">: ${blog.shift_id}</div>
                 </div>
                 <div class="form-submit cancellation-btns">         
                   <button  class="confirmCancel" onclick="setCancellationModal('${
-            blog.id
-            }','${blog.emp_id}')">Cancel Seat</button>
-                  <button  class="checkInBtn"  onclick="checkinUser('${blog.id}','${
-            blog.emp_id
-            }')">Check-in</button>        
+                    blog.id
+                  }','${blog.emp_id}')">Cancel Seat</button>
+                  <button  class="checkInBtn"  onclick="checkinUser('${
+                    blog.id
+                  }','${blog.emp_id}')">Check-in</button>        
               </div>
             </div> `;
         })
         .join("");
     }
-    blogRow.insertAdjacentHTML("afterbegin", html);
-    //blogRow.appendChild(html);
-
+    // blogRow.insertAdjacentHTML("afterbegin", html);
   } catch (error) {
     console.log(error);
   }
@@ -112,20 +111,18 @@ function cancelBookedSeat(id, empId) {
 function checkinUser(id, empId) {
   let userId = +id;
   let userEmpId = +empId;
-  // cancellationModal.classList.add("show");
-  // overlayCancellation.classList.add("active");
 
   axios
     .put(`http://localhost:5000/checkin/${userId}`, { emp_id: userEmpId })
     .then((response) => {
       console.log(response.data);
       if (response.status === 200) {
-        message.innerHTML = `<p class='${response.data.success}'>${response.data.message}</p>`;
-        if (response.data.success) {
-          setTimeout(() => {
-            closeCancellationModal();
-          }, 1500);
-        }
+        signInMessage.innerHTML = `<p class='signIn-success'>${response.data.message}</p>`;
+      } else {
+        signInMessage.innerHTML = `<p class='signIn-success'>${response.data.message}</p>`;
       }
+    })
+    .catch((error) => {
+      signInMessage.innerHTML = `<p class='signIn-success'>${error}</p>`;
     });
 }
