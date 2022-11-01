678e64df4ab0f6db2b9f9e001cc9d36d9f12b15e
devganesh123 <ganesan690101@gmail.com>
2022-11-01 10:21:31 +0530

    booking module integrated

--- public/js/script.js
+++ public/js/script.js
@@ -44,7 +44,6 @@ function display(nd, nid, nsd) {
     .then((res) => {
       console.log(res);
       $("#cont").empty();
-
       let colTable = res.data.wings;
       console.log(colTable);
       colTable.map((item) => {
@@ -64,5 +63,179 @@ function display(nd, nid, nsd) {
           }
         });
       });
+    })
+    .then(bookingModule)
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //booking moudle starts
+}
+
+// booking module functinality
+let availableModal = document.getElementById("modal-section-available");
+let occupiedModal = document.getElementById("modal-section-occupied");
+
+let closeBtnAvailable = document.querySelector("#close-btn");
+let closeBtnOccupied = document.querySelector("#close-btn-occupied");
+
+let overlayAvailable = document.getElementById("overlay");
+let overlayOccupied = document.getElementById("overlay-occupied");
+
+let message = document.querySelector("#message");
+
+function bookingModule() {
+  let availableSeatItems = document.querySelectorAll(".chair");
+  // let occupiedSeatItems = document.querySelectorAll(".chair.occupied");
+
+  availableSeatItems.forEach((item) => {
+    item.onclick = () => setAvailableModal(item);
+  });
+
+  //occupiedSeatItems.forEach((item) => {
+  //   item.onclick = () => setOccupiedModal();
+  // });
+}
+
+function setAvailableModal(seatItem) {
+  console.log(seatItem);
+  let submitBtn = document.querySelector("#submit-btn");
+  // open the modal
+  availableModal.classList.add("show");
+  overlayAvailable.classList.add("active");
+
+  // let userEmpName = seatItem.querySelector(".emp-name");
+  // let userSeatName = seatItem.querySelector(".seat-name");
+  let userSeatId = seatItem.getAttribute("id");
+  console.log(userSeatId);
+
+  // let dataSeatName = userSeatName.dataset.seatName;
+  // let dataSeatId = userSeatName.dataset.seatId;
+
+  //  insert desk id
+  let deskInput = document.querySelector("#desk-id");
+  deskInput.value = userSeatId;
+  //deskInput.dataset.seatId = dataSeatId;
+
+  //  insert Emp id
+  let deskEmployee = document.querySelector("#emp-id");
+
+  /* getting empId using datalist starts */
+  const dataList = document.getElementById("empIds");
+  const dataListInput = document.getElementById("emp-id");
+
+  const getSelecteOptionLocation = () => {
+    for (let i = 0; i < dataList.options.length; i++) {
+      if (dataList.options[i].value === dataListInput.value) {
+        return dataList.options[i];
+      }
+    }
+  };
+
+  dataListInput.addEventListener("change", () => {
+    const selectedOption = getSelecteOptionLocation();
+    if (selectedOption == undefined) {
+      console.log("option not included in the datalist");
+    } else {
+      let dataListEmpId = selectedOption.getAttribute("employeeId");
+      deskEmployee.dataset.empRefId = dataListEmpId;
+    }
+  });
+
+  /* getting empId using datalist ends */
+
+  // form submit function
+  submitBtn.onclick = () => {
+    postData(seatItem);
+  };
+}
+
+function postData(seat) {
+  //post endpoint
+  const postUrl = "http://localhost:5000/booking";
+  let desk_id = document.getElementById("desk-id").value;
+  let emp_id = document.getElementById("emp-id").dataset.empRefId;
+  let date = document.getElementById("date").value;
+  let shift = document.getElementById("time").value;
+  let bookingType = +document.querySelector('input[name="bookingType"]:checked')
+    .value;
+
+  console.log(desk_id, emp_id, date, shift, bookingType);
+
+  const payload = {
+    desk_id: `${desk_id}`,
+    emp_id: `${emp_id}`,
+    date: `${date}`,
+    shift: `${shift}`,
+    booked_by: 1,
+    booking_type: `${bookingType}`,
+  };
+
+  console.log(payload);
+
+  axios
+    .post(postUrl, payload)
+    .then((response) => {
+      // console.log(response);
+      // console.log(response.data);
+      if (response.status === 200) {
+        message.innerHTML = `<p class='${response.data.success}'>${response.data.message}</p>`;
+        if (response.data.success) {
+          setTimeout(() => {
+            seatBooking(seat);
+          }, 1500);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
+
+//close the modal
+closeBtnAvailable.addEventListener("click", closeAvailableModal);
+function closeAvailableModal() {
+  availableModal.classList.remove("show");
+  overlayAvailable.classList.remove("active");
+  //reset the input values
+  document.getElementById("desk-id").value = "";
+  document.getElementById("emp-id").value = "";
+  document.getElementById("time").value = "select";
+  message.innerHTML = "";
+}
+
+function seatBooking(seat) {
+  // close the modal when the response is sent
+  availableModal.classList.remove("show");
+  overlayAvailable.classList.remove("active");
+  message.innerHTML = "";
+
+  // availableSeatItems.forEach((item) => {
+  //   item.classList.remove("booked");
+  // });
+
+  seat.classList.add("booked");
+}
+
+function userName() {
+  axios.get(`http://localhost:5000/booking/user/name`).then((response) => {
+    console.log(response.data);
+  });
+}
+
+userName();
+
+/* occupied modal starts */
+function setOccupiedModal() {
+  // open the modal
+  occupiedModal.classList.add("show");
+  overlayOccupied.classList.add("active");
+}
+//close the modal
+closeBtnOccupied.addEventListener("click", closeOccupiedModal);
+function closeOccupiedModal() {
+  occupiedModal.classList.remove("show");
+  overlayOccupied.classList.remove("active");
+}
+/* occupied modal starts */
+// Display current Date
