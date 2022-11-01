fed7bb841464061dab57c3506f2504b5f939abe9
devganesh123 <ganesan690101@gmail.com>
2022-11-01 09:29:15 +0530

    script.js added

--- public/js/script.js
+++ public/js/script.js
@@ -1,78 +1,68 @@
-function display() {
-    let show = document.getElementById('cont');
-    $('#cont').empty();
-    let select = document.querySelector('#wing').value;
-  
-    if (select == 'WS101') {
-      // if(select =)
-      axios.get("http://127.0.0.1:5000/availability/wing/1")
-        .then(response => {
-          // let res = response.data.availability[0].wings[0].tables[0].seats;
-          let res = response.data.availability[0].wings[0].tables[0].seats;
-  
-          let colTable = response.data.availability[0].wings[0].tables;
-          colTable.map(item => {
-            let tableCol = show.innerHTML += `<div class ="table" id="table-${item.id}"></div>`;
-            item.seats.map(seat => {
-              if (document.querySelector(`#table-${item.id}`)) {
-  
-                document.querySelector(`#table-${item.id}`).innerHTML += `<div class="chair" id=${seat.seatsId} ></div>`
-              }
-              var list = document.getElementById(`${seat.seatsId}`);
-              if (seat.availability == "1") {
-                list.classList.add("booked");
-              } else if (seat.availability == "2") {
-                list.classList.add("danger");
-              }
-  
-            })
-          })
-  
-        })
-    }
-    let option = document.querySelector('#wing').value;
-    if (select == 'WS102') {
-      axios.get("http://127.0.0.1:5000/availability/wing/2")
-        .then(response => {
-          // let res = response.data.availability[0].wings[0].tables[0].seats;
-          let res = response.data.availability[0].wings[0].tables[0].seats;
-  
-          let colTable = response.data.availability[0].wings[0].tables;
-          colTable.map(item => {
-            let tableCol = show.innerHTML += `<div class ="table" id="table-${item.id}"></div>`;
-            item.seats.map(seat => {
-              if (document.querySelector(`#table-${item.id}`)) {
-  
-                document.querySelector(`#table-${item.id}`).innerHTML += `<div class="chair" id=${seat.seatsId} ></div>`
-              }
-              var list = document.getElementById(`${seat.seatsId}`);
-              if (seat.availability == "1") {
-                list.classList.add("booked");
-              } else if (seat.availability == "2") {
-                list.classList.add("danger");
-              }
-  
-            })
-          })
-  
-        })
-    }
-  }
-  // function add() {
-  //   var list = document.getElementById("WS-Seat").classList;
-  //   list.add("available");
-  // }
-  
-  const store = document.querySelector("#wing");
-  store.addEventListener("change", filter);
-  
-  function filter() {
-    let filterValue = store.value.toLowerCase();
-    `${filterValue}`.innerHTML = display();
-  
-  }
-  
-  let date = document.querySelector('input').value;
-  
-  
-  
+var date = new Date();
+var currentDate = date.toISOString().slice(0, 10);
+document.getElementById("date").value;
+let ename = document.getElementById("emp");
+let show = document.getElementById("cont");
+
+let cdate = document.getElementById("date");
+let cwing = document.getElementById("wing");
+let cshift = document.getElementById("shift");
+let cButton = document.getElementById("check");
+let nd;
+let nid;
+let nsd;
+
+cdate.addEventListener("change", (event) => {
+  let dd = event.target.value;
+  nd = dd;
+  console.log(nd);
+});
+
+cwing.addEventListener("change", (event) => {
+  let id = event.target.value;
+  nid = id;
+  console.log(nid);
+});
+
+cshift.addEventListener("change", (event) => {
+  let sd = event.target.value;
+  nsd = sd;
+  console.log(nid);
+});
+
+cButton.addEventListener("click", () => {
+  display(nd, nid, nsd);
+});
+
+function display(nd, nid, nsd) {
+  axios
+    .post(`http://localhost:5000/availability/`, {
+      wing: nid,
+      date: nd,
+      shift: nsd,
+    })
+    .then((res) => {
+      console.log(res);
+      $("#cont").empty();
+
+      let colTable = res.data.wings;
+      console.log(colTable);
+      colTable.map((item) => {
+        let tableCol =
+          (show.innerHTML += `<div class ="table" id="table-${item.tableid}"></div>`);
+        item.seats.map((seat) => {
+          if (document.querySelector(`#table-${item.tableid}`)) {
+            document.querySelector(
+              `#table-${item.tableid}`
+            ).innerHTML += `<div class="chair" id=${seat.seatid} ></div>`;
+          }
+          var list = document.getElementById(`${seat.seatsId}`);
+          if (seat.availability == "1") {
+            list.classList.add("booked");
+          } else if (seat.availability == "2") {
+            list.classList.add("danger");
+          }
+        });
+      });
+    });
+}
